7bd2b1ebfa995b963e07ef088086ae2764611a1b
glyph <nosolonin@gmail.com>
2018-12-28 02:44:24 +0300

    read from console
    and display
    Not what i want at all. Need to use wianpi i guess :(

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Welcome to BPM counter!");
     println!("Press 'space' to count BPM, 'r' to restart or 'q' to quit.");
+    let mut input = String::new();
+    loop {
+        io::stdin().read_line(&mut input).expect("failed to read line");
+        println!("your input: {}", input);
+    }
 }
